b3aeec3560a98bd56cbfc6191308c421dc86390e
LiuTao <mat.liutos@gmail.com>
2012-06-28 23:16:11 +0800

    PE第27题，不知道为什么总是错。

--- lisp/pro27.lisp
+++ lisp/pro27.lisp
@@ -1,14 +1,49 @@
 (proclaim '(optimize speed))
 
-(defun primep (number)
-  (labels ((rec (test)
-	     (cond ((> (* test test) number) t)
-		   ((= 0 (rem number test)) nil)
-		   (t (rec (1+ test))))))
-    (cond ((= 1 number) nil)
-	  ((= 2 number) t)
-	  (t (rec 2)))))
+(defparameter *primes*
+  (make-hash-table))
 
-(defun cons-fn (a b)
-  (lambda (n)
-    (+ (* n n) (* a n) b)))
+(defun primep (n)
+  (declare (type integer n))
+  (the boolean
+    (multiple-value-bind (value found)
+	(gethash n *primes*)
+      (if found value
+	  (let ((value (cond ((>= 1 n) nil)
+			     ((= 2 n) t)
+			     (t (let ((limit (truncate (sqrt n))))
+				  (labels ((aux (test)
+					     (cond ((> test limit) t)
+						   ((zerop (rem n test)) nil)
+						   (t (aux (1+ test))))))
+				    (aux 2)))))))
+	    (when value
+	      (setf (gethash n *primes*) t))
+	    value)))))
+
+(defun make-fn (a b)
+  (declare (type (integer -999 999) a b))
+  (the function
+    (lambda (n)
+      (+ (* n (+ n a)) b))))
+
+(defun prime-count (fn)
+  (declare (type function fn))
+  (the fixnum
+    (labels ((aux (acc n)
+	       (if (primep (funcall fn n))
+		   (aux (1+ acc) (1+ n))
+		   acc)))
+      (aux 0 0))))
+
+(defun pro27 ()
+  (let (max
+	(win 0))
+    (dotimes (a 1999)
+      (dotimes (b 1000)
+	(let ((a (- a 999)))
+	  (let ((count (prime-count (make-fn a b))))
+	    (when (> count win)
+	      (setf max (* a b)
+		    win count))))))
+    (values max win)))
