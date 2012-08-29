2ea97a8e8f0e664cd27038d0a1b483a85ab8a7c0
LiuTao <mat.liutos@gmail.com>
2012-08-29 22:34:16 +0800

    改正了第17题，加上了对and单词的修正。

--- lisp/pro17.lisp
+++ lisp/pro17.lisp
@@ -2,8 +2,10 @@
   (let ((cnt 0))
     (loop
        :for i from 1 upto n
-       :do (incf cnt (count-if #'(lambda (c)
-				   (and (char/= #\Space c)
-					(char/= #\- c)))
-			       (format nil "~R" i))))
+       :do (incf cnt
+		 (+ (count-if #'(lambda (c)
+				  (and (char/= #\Space c)
+				       (char/= #\- c)))
+			      (format nil "~R" i))
+		    (if (and (> i 100) (/= 0 (rem i 100))) 3 0))))
     cnt))
